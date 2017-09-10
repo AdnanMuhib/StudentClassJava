474ae70ba3ab6269e5adcf6d031272769384c9ed
Adnan Muhib <adnan.muhib@rocketmail.com>
2017-09-11 00:25:09 +0500

    Completed implementation as described in Description
    Completed implementation as described in Description
    with some examples in the main function which is part of Test.java file
    used Date libraries to handle the Date of Birth and Calculate the Age of
    Student.

---
+++ src/test/Test.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package test;
+
+
+/**
+ *
+ * @author Muhammad Adnan Mohib
+ */
+public class Test {
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Student std = new Student();
+        std.setStudentName("Sarfraz Ahmad");
+        std.setRegNumber("2017-CS-999");
+        std.setCGPA(2.9);
+        std.setDOB("December/27/1999");
+        std.setCNIC("3567912356781");
+        std.displayStudent();
+        System.out.println();
+        Student HafizAslam = new Student("Hafiz Muhammad Aslam", "2017-CS-999");
+        HafizAslam.setCGPA(3.4);
+        HafizAslam.setDOB("November/27/1999");
+        HafizAslam.setCNIC("3567912356781");
+        HafizAslam.displayStudent();
+    }
+    
+}

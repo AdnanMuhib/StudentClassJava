9844b4b65df13ea21283c5d6a375d1b8536000f9
Adnan Muhib <adnan.muhib@rocketmail.com>
2017-09-11 01:15:29 +0500

    Implementation of Destructor
    Overriding the finalize() method the garbage collector of the Java.

--- src/test/Test.java
+++ src/test/Test.java
@@ -16,19 +16,34 @@ public class Test {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        Student std = new Student();
+        // decalring Objects of Student Class
+        Student std = new Student();;
+        Student HafizAslam = new Student("Hafiz Muhammad Aslam", "2017-CS-999");
+        try{
+        
         std.setStudentName("Sarfraz Ahmad");
         std.setRegNumber("2017-CS-999");
         std.setCGPA(2.9);
         std.setDOB("December/27/1999");
         std.setCNIC("3567912356781");
         std.displayStudent();
+        
         System.out.println();
-        Student HafizAslam = new Student("Hafiz Muhammad Aslam", "2017-CS-999");
+        
         HafizAslam.setCGPA(3.4);
         HafizAslam.setDOB("November/27/1999");
         HafizAslam.setCNIC("3567912356781");
         HafizAslam.displayStudent();
+        }
+        // catching any errors 
+        catch(Throwable t){
+            throw t;
+        }
+        // calling destructor
+        finally{
+            std.finalize();
+            HafizAslam.finalize();
+        }
     }
     
 }
